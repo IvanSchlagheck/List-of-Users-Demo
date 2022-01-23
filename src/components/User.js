0e0d6724ebae1dd90a6b07657572f9c6490562c4
ivan schlagheck <ivansch05@gmail.com>
2022-01-23 15:48:50 +0100

    User Class component created

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,15 @@
-import classes from './User.module.css';
+import { Component } from "react";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+import classes from "./User.module.css";
+
+class User extends Component { //Inherits the functionality of the class Component that is already given and defined by React
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>; // props comes with component but to access it we need to use the keyword .this
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
